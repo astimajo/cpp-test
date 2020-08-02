139c0f78e4a8f55fda17ee17716fd025f8335239
astimajo <64699717+astimajo@users.noreply.github.com>
2020-08-03 07:31:15 +0800

    Add files via upload

---
+++ hello.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+    string message = "Hello World!\n"; //string now exists as a legitimate data type.
+
+    cout << message; //simpler syntax to output to screen.
+}
